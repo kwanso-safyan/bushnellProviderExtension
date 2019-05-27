41f226f121ec19a55deca32dbef4e8ef23e1352f
safyan-kwanso <safyan.jamil@kwanso.com>
2019-05-27 14:44:22 +0500

    logics updated.

---
+++ bushnellProviderExtension.podspec
@@ -0,0 +1,35 @@
+Pod::Spec.new do |s|
+
+s.name         = "bushnellProviderExtension"
+s.version      = "1.0.0"
+s.summary      = "This is such a Bushnell Provider framework, Kwanso."
+s.description  = "This is some super Bushnell Provider framework that was made by Kwanso."
+s.license      = "MIT"
+s.author       = { "Safyan Jamil" => "safyan.jamil@kwanso.com" }
+s.platform     = :ios, "11.0"
+s.homepage      = "https://github.com/kwanso-safyan/bushnellProviderExtension"
+s.source       = { :git => "https://github.com/kwanso-safyan/bushnellProviderExtension.git", :tag => "1.0.0" }
+s.source_files  = "bushnellProviderExtension/**/*.{h,m,swift,xcdatamodeld}"
+
+#s.framework  = "Alamofire"
+s.frameworks = "Alamofire", "CommonCryptoModule"
+
+
+
+# ――― Project Settings ――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
+#
+#  If your library depends on compiler flags you can set them in the xcconfig hash
+#  where they will only apply to your library. If you depend on other Podspecs
+#  you can include multiple dependencies to ensure it works.
+
+# s.requires_arc = true
+
+# s.xcconfig = { "HEADER_SEARCH_PATHS" => "$(SDKROOT)/usr/include/libxml2" }
+s.dependency "Alamofire", "4.8.0"
+s.dependency "CommonCryptoModule", "1.0.2"
+s.dependency "IQKeyboardManagerSwift", "6.2.0"
+s.dependency "MBProgressHUD", "1.1.0"
+s.dependency "ReachabilitySwift", "4.3.0"
+s.dependency "SwiftyJSON", "4.2.0"
+
+end
--- bushnellProviderExtension/Classes/Constants.swift
+++ bushnellProviderExtension/Classes/Constants.swift
@@ -12,68 +12,20 @@ import Foundation
 // Shared instance shortcuts
 
 let NOTIFICATION_CENTER = NotificationCenter.default
-let FILE_MANAGER = FileManager.default
 let MAIN_BUNDLE = Bundle.main
 let MAIN_THREAD = Thread.main
 let MAIN_SCREEN = UIScreen.main
 let USER_DEFAULTS = UserDefaults.standard
 let APPLICATION = UIApplication.shared
 let CURRENT_DEVICE = UIDevice.current
-let MAIN_RUN_LOOP = RunLoop.main
-let GENERAL_PASTEBOARD = UIPasteboard.general
-let CURRENT_LANGUAGE = (Locale.current as NSLocale).object(forKey: NSLocale.Key.languageCode)!
 
-// Network
-let NETWORK_ACTIVITY = APPLICATION.isNetworkActivityIndicatorVisible
 // Color consts
 let CLEAR_COLOR = UIColor.clear
-// Application informations
-let APPLICATION_NAME = MAIN_BUNDLE.infoDictionary?[kCFBundleNameKey as String]
-let APPLICATION_VERSION = MAIN_BUNDLE.object(forInfoDictionaryKey: "CFBundleVersion")
-let IN_SIMULATOR = (TARGET_IPHONE_SIMULATOR != 0)
-
 
 // UserDefauld Keys
 let IS_USER_LOGGEDIN = "IS_USER_LOGGEDIN"
 let SESSION_EXPIRY_DATE = "SESSION_EXPIRY_DATE"
 
-// NSNotifcation
-
-let SUCCESS_DEEPLINK_CODE = "SUCCESS_DEEPLINK_CODE_TOKEN_API"
-let FAIL_DEEPLINK_CODE = "FAIL_DEEPLINK_CODE_TOKEN_API"
-let NO_DEEPLINK_CODE = "NO_DEEPLINK_CODE_TOKEN_API"
-
-// Storyboards Identfiers
-
-let LOGIN_IDENTIFIER = "LoginViewController"
-let DASHBOARD_IDENTIFIER = "DashboardViewController"
-let PROFILE_IDENTIFIER = "ProfileViewController"
-
-// Msssages
-
-let CELLULAR_TITLE = "Cellular Network"
-let CELLULAR_CONFIRMATION_MSG = "Are you sure you want to continue with cellular data?"
-let NETWORK_SUCCESS = "Please connect with internet"
-let NETWORK_TITLE = "Network Failure!"
-let CONFIRMATION = "Confirmation!"
-let LOGOUT_MSG = "Are you sure you want to logout?"
-let LOGOUT_BTN_TEXT = "Logout"
-
-let SUCCESS_TITLE = "Success"
-let ALERT_TITLE = "Alert"
-let WARNING_TITLE = "Warning"
-let ERROR_DESCRIPTION = "error_description"
-let SESSION_EXPIRE_MSG = "User session has expired."
-let ALL_FIELD_MSG = "Please fill all fields."
-
-let LOCATION_PATH = "App-Prefs:root=Privacy&path=LOCATION"
-let TURN_ON_LOCATION_TITLE = "Turn on Location Services"
-let TURN_ON_LOCATION_MSG = "1. Tap Settings \n2. Tap Location \n3. Tap While Using the App"
-let NOT_NOW = "Not Now"
-let SETTINGS = "Settings"
-let OK = "Ok"
-
-let PROFILE_UPDATE_MSG = "Your profile is successfully updated."
 
 
 
